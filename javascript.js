b71e997f27e0a585f7500bfb33b10efef36a890b
Rakshit Ambi <rakshitambi7a@gmail.com>
2022-12-04 12:57:18 +0530

    added external js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function pr(){
+    document.getElementById('usr').value="CipherSchools"
+}
+var a,b,sum,sub,div 
+a = 10;
+b = 20;
+sum = a + b;
+sub = a - b;
+div = a / b;
+
+console.log(sum);
+document.getElementById('sum').
+innerHTML= sum;
+
+document.getElementById('diff').innerHTML =  sub;
+document.getElementById('div').innerHTML =  div;
+document.getElementById('multi').innerHTML =  a*b;
+document.getElementById('tect').innerHTML =  a*b;
+
